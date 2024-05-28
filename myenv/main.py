4df3d3f00f2594f405072de4eae7480ec310928d
Aliyah Sausan Huwel <ahuwel@Aliyahs-Air.lan>
2024-05-28 19:44:59 -0400

    update TokTok

---
+++ myenv/main.py
@@ -0,0 +1,135 @@
+import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+
+# Google Sheets setup
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("newyorkah/myenv/toktok-424721-72c43ebbe7bd.json", scope)
+client = gspread.authorize(creds)
+
+# Open the Google Sheet
+sheet_id = "1BlMHQWPJnEgEWtwfNhi7Kuv-khzz4BA3y--DoKFgddk"
+sheet = client.open_by_key(sheet_id)
+
+st.title("TokTok: Cause finding apartments in US is a painful experience")
+
+menu = ["Home", "Apartment Checking", "Roommate Matching", "Decoration"]
+choice = st.sidebar.selectbox("Click dropdown for our services", menu)
+
+if choice == "Home":
+    st.subheader("Welcome to the International Student Apartment Helper")
+    st.write("""
+    **Our Purpose:**
+    
+    This platform is designed to assist international students with their apartment hunting journey. 
+    We understand that finding an apartment, especially from abroad, can be challenging. 
+    Our goal is to make this process easier and more efficient by providing the following services:
+
+    1. **Apartment Checking:** We understand that you are not sure whether you can trust the agent or the apartment you see on the website. Submit details about the apartments you are interested in, and we will help check and record information for you via WhatsApp.
+   
+    2. **Roommate Matching:** Find a new roommate to live together for months? But, do you know what to ask to them to know that they are the right ones? Fill out a questionnaire about your preferences, and we will help you find potential roommates.
+    
+    3. **Apartment Decoration:** You are about to spend good amount of hours in your room, wandering why you are doing this to yourself so make sure that you will do it in aesthetic condition- at least. Share your Pinterest board with us, and we will provide personalized decoration suggestions and help to decorate! And Hey, we might find  cheapeer stuffs for your room ;) 
+
+    **Pricing**
+    1. **Apartment Checking:** 15-20 dollars per 1 apartment view depending on distance
+   
+    2. **Roommate Matching:** 10 dollars per 1 roommate candidate
+    
+    3. **Apartment Decoration:** 25 dollars per 1 room
+
+    We hope this platform helps make your transition to a new home smoother and more enjoyable!
+
+    PIC: 
+    +1-332-251-1844 - Sausan Huwel
+    """)
+elif choice == "Apartment Checking":
+    st.subheader("Submit Apartment Details")
+    with st.form("apartment_form"):
+        name = st.text_input("Name")
+        email = st.text_input("Email")
+        number = st.text_input("WhatsApp Number")
+        apt_location = st.text_input("Location")
+        apt_agent_number = st.text_input("Apartment Agent/Broker Phone Number")
+        apt_link= st.text_input("Link to The Apartment Location")
+        questions= st.text_input("Questions or Things to Check")
+        submit = st.form_submit_button("Submit")
+
+        if submit:
+            sheet_apt = sheet.worksheet("Apartment Checking")
+            sheet_apt.append_row([name, email, number, apt_location, apt_agent_number, apt_link, questions])
+            st.success("Submitted successfully")
+
+elif choice == "Roommate Matching":
+    st.subheader("Submit Roommate Preferences")
+    with st.form("roommate_form"):
+        name = st.text_input("Name")
+        email = st.text_input("Email")
+        number = st.text_input("WhatsApp Number")
+
+        lifestyle = st.radio(
+            "Which statement best describes you?",
+            ("I am a morning person and prefer quiet in the evenings", "I am a night person and prefer quiet in the morning", "I am adaptable and don't have a strong preference for quiet time")
+        )
+
+        cooking = st.radio(
+            "How often do you plan on cooking and eating in the apartment?",
+            ("All the time", "Most of the time", "Rarely", "Never")
+        )
+
+        guest = st.radio(
+            "How often would you like to have guests over to your apartment?",
+            ("Frequently", "Occasionally", "Rarely", "Never, but I don't mind if my roommate does")
+        )
+
+        other_people = st.radio(
+            "Are you okay if someone outside of your gender comes over to the apartment?",
+            ("I am not okay", "I am okay as long as my roommate gives me a heads-up", "I am okay")
+        )
+
+        cleanliness = st.radio(
+            "How often do you clean your living space?",
+            ("Daily", "Weekly", "Bi-weekly", "Monthly or more infrequently")
+        )
+
+        dietary = st.radio(
+            "Do you have any special dietary preferences?",
+            ("None", "Vegetarian or Vegan", "Kosher", "Halal")
+        )
+
+        shared_utensils = st.radio(
+            "Are you okay with sharing utensils?",
+            ("I am not okay", "I am okay as long as it is shared with someone with the same dietary requirements as me", "I am okay as long as my roommate cleans them", "I am okay")
+        )
+
+        relationship = st.radio(
+            "What are your expectations surrounding roommate friendship?",
+            ("I don't expect to be close friends, just coexist peacefully", "I am open to naturally developing a good friendship", "I envision becoming close friends and spending time together")
+        )
+
+        preferences = st.text_area("Other Preferences")
+        submit = st.form_submit_button("Submit")
+
+        if submit:
+            sheet_rm = sheet.worksheet("Roommate Matching")
+            sheet_rm.append_row([name, email, number, lifestyle, cooking, guest, other_people, cleanliness, dietary, shared_utensils, relationship, preferences])
+            st.success("Submitted successfully")
+
+elif choice == "Decoration":
+    st.subheader("Submit Pinterest Board")
+    with st.form("decoration_form"):
+        name = st.text_input("Name")
+        email = st.text_input("Email")
+        number = st.text_input("WhatsApp Number")
+        pinterest_link = st.text_input("Pinterest Board Link")
+        decor_preference = st.radio(
+            "How do you want to handle the furnitures?",
+            ("I want to buy new furniture myself", "I want to buy secondhand furniture myself", "I want TokTok to buy new furniture", "I want TokTok to buy secondhand furniture")
+        )
+        submit = st.form_submit_button("Submit")
+
+        if submit:
+            sheet_dec = sheet.worksheet("Decoration")
+            sheet_dec.append_row([name, email, pinterest_link, decor_preference])
+            st.success("Submitted successfully")
