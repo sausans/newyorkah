4fef670130edce2315412b32b758f0181d8750ee
Aliyah Sausan Huwel <33594713+sausans@users.noreply.github.com>
2024-05-29 16:35:08 -0400

    Update main.py

--- myenv/main.py
+++ myenv/main.py
@@ -8,6 +8,7 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 import tempfile
+from PIL import Image
 
 def upload_to_drive(file, credentials_json):
     # Save the uploaded file to a temporary location
